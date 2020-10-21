426209b6ca9893719296c49173c594f1d1428a09
Vanessa D'Alessandro <leavesthatfall@gmail.com>
2020-10-21 18:15:48 +0200

    WIP

--- js/script.js
+++ js/script.js
@@ -1,12 +1,38 @@
 var nome=document.getElementById("nome");
-var km=document.getElementById("km");
+var km;
 var genera=document.getElementById("genera");
 var annulla=document.getElementById("annulla");
 var eta=document.getElementById("eta");
+var prezzoKm=0.21;
+var subtotale
 
 genera.addEventListener("click", 
     function(){
+        km=parseInt(document.getElementById("km").value)
+        subtotale=(prezzoKm * km).toFixed(2)
+        document.getElementById("container-risultato").style.display = "inline"
         if (eta.value==="minorenne"){
-            console.log(eta.value)
+            var costoUno=(subtotale-((subtotale/100)*20)).toFixed(2)
+            document.getElementById("costo").innerHTML=costoUno;
+            document.getElementById("utente").innerHTML=nome.value;
+            document.getElementById("offerta").innerHTML="Sconto Minorenne";
+            document.getElementById("carrozza").innerHTML=Math.floor(Math.random() * 10);
+            document.getElementById("codice").innerHTML=Math.floor(Math.random() * 100000);
+            
+        } else if(eta.value==="over65"){
+            var costoDue=(subtotale-((subtotale/100)*40)).toFixed(2)
+            document.getElementById("costo").innerHTML=costoDue;
+            document.getElementById("utente").innerHTML=nome.value;
+            document.getElementById("offerta").innerHTML="Sconto Over 65";
+            document.getElementById("carrozza").innerHTML=Math.floor(Math.random() * 10);
+            document.getElementById("codice").innerHTML=Math.floor(Math.random() * 100000);
+        } else{
+            var costoTre=subtotale;
+            document.getElementById("costo").innerHTML=costoTre;
+            document.getElementById("utente").innerHTML=nome.value;
+            document.getElementById("offerta").innerHTML="No sconto";
+            document.getElementById("carrozza").innerHTML=Math.floor(Math.random() * 10);
+            document.getElementById("codice").innerHTML=Math.floor(Math.random() * 100000);
         }
     })
+
