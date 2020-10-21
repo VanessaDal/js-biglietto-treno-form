e5da749c00e15b45e19aaf8b18f86b7db332282a
Vanessa D'Alessandro <leavesthatfall@gmail.com>
2020-10-21 22:51:15 +0200

    the eeeend

--- js/script.js
+++ js/script.js
@@ -13,26 +13,26 @@ genera.addEventListener("click",
         document.getElementById("container-risultato").style.display = "block";
         if (eta.value==="minorenne"){
             var costoUno=(subtotale-((subtotale/100)*20)).toFixed(2)
-            document.getElementById("costo").innerHTML=costoUno;
+            document.getElementById("costo").innerHTML=costoUno + " €";
             document.getElementById("utente").innerHTML=nome.value;
             document.getElementById("offerta").innerHTML="Sconto Minorenne";
-            document.getElementById("carrozza").innerHTML=Math.floor(Math.random() * 10);
-            document.getElementById("codice").innerHTML=Math.floor(Math.random() * 100000);
+            document.getElementById("carrozza").innerHTML=Math.floor(Math.random() * 10+1);
+            document.getElementById("codice").innerHTML=Math.floor(Math.random() * 10000+90000);
             
         } else if(eta.value==="over65"){
             var costoDue=(subtotale-((subtotale/100)*40)).toFixed(2)
-            document.getElementById("costo").innerHTML=costoDue;
+            document.getElementById("costo").innerHTML=costoDue + " €";
             document.getElementById("utente").innerHTML=nome.value;
             document.getElementById("offerta").innerHTML="Sconto Over 65";
-            document.getElementById("carrozza").innerHTML=Math.floor(Math.random() * 10);
-            document.getElementById("codice").innerHTML=Math.floor(Math.random() * 100000);
+            document.getElementById("carrozza").innerHTML=Math.floor(Math.random() * 10+1);
+            document.getElementById("codice").innerHTML=Math.floor(Math.random() * 10000+90000);
         } else{
             var costoTre=subtotale;
-            document.getElementById("costo").innerHTML=costoTre;
+            document.getElementById("costo").innerHTML=costoTre + " €";
             document.getElementById("utente").innerHTML=nome.value;
             document.getElementById("offerta").innerHTML="No sconto";
-            document.getElementById("carrozza").innerHTML=Math.floor(Math.random() * 10);
-            document.getElementById("codice").innerHTML=Math.floor(Math.random() * 100000);
+            document.getElementById("carrozza").innerHTML=Math.floor(Math.random() * 10+1);
+            document.getElementById("codice").innerHTML=Math.floor(Math.random() * 10000+90000);
         }
     })
 
